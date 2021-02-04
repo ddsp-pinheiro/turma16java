7ed46a137af01c83ec0581fc4dc3b6730bf473d7
Daniele Pinheiro <ddsp.pinheiro@gmail.com>
2021-02-04 20:13:07 -0300

    classe abstrata salva

--- java/aulas/src/classeAbstrata/Preguiça.java
+++ java/aulas/src/classeAbstrata/Preguiça.java
@@ -1,5 +1,20 @@
 package classeAbstrata;
 
-public class Preguiça {
+public class Preguiça extends Animal 
+{
+
+	public Preguiça(String nome, int idade) {
+		super(nome, idade);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String som() {
+		
+		return "(preguiça não tem barulho caracteristico)";
+	}
+	public void subirAvore() {
+		
+	}
 
 }
